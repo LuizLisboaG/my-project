8b8b8549b1e7e4fb06e746f611c29193b59c3f6f
LuizLisboaG <luizlisboag@gmail.com>
2022-02-03 15:08:36 -0300

    Implementação dos V-CARDS em um V-FOR

--- src/App.vue
+++ src/App.vue
@@ -14,35 +14,68 @@
       <!--  -->
     </v-navigation-drawer>
 
-    <v-main class="grey lighten-2">
-      <v-container>
-        <v-row>
-          <template v-for="n in 4">
-            <v-col
-              :key="n"
-              class="mt-2"
-              cols="12"
+    <v-main class="background">
+      <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
             >
-              <strong>Category {{ n }}</strong>
-            </v-col>
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
 
-            <v-col
-              v-for="j in 6"
-              :key="`${n}${j}`"
-              cols="6"
-              md="2"
+            <v-card-actions 
+            height="200px"
             >
-              <v-sheet height="150"></v-sheet>
-            </v-col>
-          </template>
-        </v-row>
-      </v-container>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({
+      cards: [
+        { title: 'Natal', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
+        { title: 'Mossoró', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'Caicó', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+        { title: 'Natal', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+        { title: 'Natal', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
+        { title: 'Mossoró', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'Caicó', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+        { title: 'Natal', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+      ],
+    }),
   }
 </script>
+
+<style scoped>
+.background{
+  background: rgb(237,111,29);
+  background: linear-gradient(90deg, rgba(237,111,29,1) 0%, rgba(16,39,50,1) 100%);
+}
+</style>
